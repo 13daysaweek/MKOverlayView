fb789082087a9618be80212b87547813f4c0e743
Christopher House <chris.house.00@gmail.com>
2013-05-09 18:02:26 -0500

    Some stuff changed...I guess

--- ThirteenDaysAWeek.MKOverlayView/Views/MainView.cs
+++ ThirteenDaysAWeek.MKOverlayView/Views/MainView.cs
@@ -12,28 +12,19 @@ namespace ThirteenDaysAWeek.MKOverlayView.Views
 			this.SetupView();
 		}
 
-		public override void LayoutSubviews ()
-		{
-			base.LayoutSubviews ();
-
-			this.Toolbar.Frame = new RectangleF(0, 0, this.Frame.Width, this.Frame.Height);
-			this.MapView.Frame = new RectangleF(0, 44, this.Frame.Width, this.Frame.Height - 44);
-			this.StatesPickerView.Frame = new RectangleF(0, 500, this.Frame.Width, 180);
-		}
-
 		private void SetupView()
 		{
 			this.Toolbar = new UIToolbar(new RectangleF(0, 0, this.Frame.Width, this.Frame.Height));
 			this.MapView = new MKMapView(new RectangleF(0, 44, this.Frame.Width, this.Frame.Height - 44));
 			this.MapView.Delegate = new MapDelegate();
 
+			this.StatesPickerView = new UIPickerView(new RectangleF(900,44,this.Frame.Width, 180));
+			this.StatesPickerView.ShowSelectionIndicator = true;
+
 			UIBarButtonItem statesButton = new UIBarButtonItem("States", UIBarButtonItemStyle.Bordered, (s,e) => {
 				this.MoveMapOutOfViewAndPickerIntoView();
 			});
-			this.Toolbar.SetItems(new UIBarButtonItem[]{statesButton}, true);
-
-			this.StatesPickerView = new UIPickerView(new RectangleF(0,500,this.Frame.Width, 180));
-			this.StatesPickerView.ShowSelectionIndicator = true;
+			this.Toolbar.Items = new UIBarButtonItem[]{statesButton};
 
 			this.AddSubview (this.Toolbar);
 			this.AddSubview(this.MapView);
@@ -52,7 +43,7 @@ namespace ThirteenDaysAWeek.MKOverlayView.Views
 			UIView.SetAnimationDuration(.3);
 			
 			this.MapView.Frame = new RectangleF(0, 44, this.Frame.Width, this.Frame.Height - 44);
-			this.StatesPickerView.Frame = new RectangleF(0, 500, this.Frame.Width, 180);
+			this.StatesPickerView.Frame = new RectangleF(900, 44, this.Frame.Width, 180);
 			
 			UIView.CommitAnimations();
 		}
