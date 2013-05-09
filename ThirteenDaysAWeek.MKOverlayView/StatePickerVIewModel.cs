55a38867ba3f2f50f4b6433f67e7080ec969c0f9
Christopher House <chris.house.00@gmail.com>
2013-05-09 08:29:45 -0500

    Added picker vm

---
+++ ThirteenDaysAWeek.MKOverlayView/StatePickerVIewModel.cs
@@ -0,0 +1,37 @@
+using System;
+using MonoTouch.UIKit;
+using System.Collections.Generic;
+
+namespace ThirteenDaysAWeek.MKOverlayView
+{
+	public class StatePickerVIewModel : UIPickerViewModel
+ 	{
+		private readonly IList<string> states;
+
+		public StatePickerVIewModel(IList<string> states)
+		{
+			this.states = states;
+		}
+
+		public override int GetComponentCount (UIPickerView picker)
+		{
+			return 1;
+		}
+
+		public override int GetRowsInComponent (UIPickerView picker, int component)
+		{
+			return this.states.Count;
+		}
+
+		public override float GetRowHeight (UIPickerView picker, int component)
+		{
+			return 44f;
+		}
+
+		public override string GetTitle (UIPickerView picker, int row, int component)
+		{
+			return this.states[row];
+		}
+	}
+}
+
